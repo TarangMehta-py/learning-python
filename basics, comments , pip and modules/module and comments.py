83ee122eed3982decd73b49ec4f9b9ef8ec25407
Tarang Mehta <mehtatarang.12102007@gmail.com>
2025-08-28 20:54:11 +0530

    Add files via upload

---
+++ basics, comments , pip and modules/module and comments.py
@@ -0,0 +1,7 @@
+#Downloaded this module through pip install.
+import pyjokes
+joke = pyjokes.get_joke()
+print(joke)
+"""The third line in the code 
+was really hard when i saw it first, 
+but it was not meant to be learnt."""
